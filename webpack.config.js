fc8e5675aa887d78c0225656f1e88c319934a811
joe bumbaca <joebumbaca@gmail.com>
2019-07-18 09:22:29 -0700

    Create webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      options: {
+        presets: ["@babel/preset-env"]
+      },
+    }]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  },
+  watch: true
+};
